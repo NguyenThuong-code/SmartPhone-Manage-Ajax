0e94ce4f957a960cd9f4952eeadd00ae87cc8c49
NguyenThuong-code <you@example.com>
2022-10-30 10:11:06 +0700

    SmartPhone

---
+++ src/main/java/com/thuong/service/ISmartphoneService.java
@@ -0,0 +1,15 @@
+package com.thuong.service;
+
+import com.thuong.model.Smartphone;
+
+import java.util.Optional;
+
+public interface ISmartphoneService {
+    Iterable<Smartphone> findAll();
+
+    Optional<Smartphone> findById(Long id);
+
+    Smartphone save(Smartphone smartPhone);
+
+    void remove(Long id);
+}
